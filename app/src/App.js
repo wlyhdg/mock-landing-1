0f37e261d66874fe8fe7e141216e0c737f45c1d1
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-11 21:21:24 -0800

    Commented out default JSX code for App, imported and inserted Navbar component

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="app">
+        <Navbar />
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -20,7 +21,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header>*/}
       </div>
     );
   }
